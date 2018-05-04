377822e131ffcf0daad3dded06d66a409049aba8
Abel <asaavedra@protonmail.com>
2018-05-04 23:03:43 +0200

    Ejercicio completado

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,6 @@
 import random
+from turtledemo.minimal_hanoi import play
+
 
 class Card:
     def __init__(self, number, suit):
@@ -33,6 +35,7 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.score = 0
+        self.win_lose_draw = ""
 
 
 class Game:
@@ -71,6 +74,12 @@ class Game:
                 total += self.card_values[card.number]
         return total
 
+    def ask_how_many_players(self):
+        number = ""
+        while not number.isdigit():
+            number = input("How many players? ")
+        return int(number)
+
     def player_wants_to_continue(self):
         response = input("Do you want another card? (Y/N)")
         return response == "Y" or response == "y"
@@ -82,22 +91,39 @@ class Game:
         return name
 
     def run(self):
-        num_players = 0
-        num_players = int(input("How many players? "))
+        num_players = self.ask_how_many_players()
 
-        for n in range(1, num_players + 1):
-            self.players.append(Player(self.ask_player_name(n)))
+        if num_players > 0:
+            for n in range(1, num_players + 1):
+                self.players.append(Player(self.ask_player_name(n)))
 
-        for player in self.players:
-            print("\nPlayer {} turn:\n".format(player.name))
-            user_continue = True
-            self.table_cards.clear()
-            while user_continue and self.count_table_cards() < 21:
-                self.draft_card()
-                user_continue = self.player_wants_to_continue()
+            max_score = 0
+            num_winner = 0
+            num = 0
+            for player in self.players:
+                print("\nPlayer {} turn:\n".format(player.name))
+                num += 1
+                user_continue = True
+                self.table_cards.clear()
+                while user_continue and self.count_table_cards() < 21:
+                    self.draft_card()
+                    user_continue = self.player_wants_to_continue()
 
-            player.score = self.count_table_cards()
-            print("Your score {}".format(player.score))
+                player.score = self.count_table_cards()
+                if player.score > 21:
+                    player.win_lose_draw = "loses"
+                else:
+                    if player.score > max_score:
+                        max_score = player.score
+                        player.win_lose_draw = "wins"
+                    else:
+                        player.win_lose_draw = "loses"
+                print("Your score {}".format(player.score))
+
+            print("\nSCORES\n------")
+            temp = sorted(self.players, key=lambda player: player.score, reverse=True)
+            for player in temp:
+                print("{} {} with {}".format(player.name, player.win_lose_draw, player.score))
 
 
 if __name__ == "__main__":
